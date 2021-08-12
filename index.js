eafc489d76ff44ca2faa04629717cda28fcfc1c6
Jethro Lee <jethro.lee@probit.com>
2021-08-12 18:47:08 +0900

    Try

--- index.js
+++ index.js
@@ -4,10 +4,10 @@
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
 process.stdin.on('data', (data) => {
-  data = data.replace(/^[^\s]+/, (match) => {
+  data = String(data).replace(/^[^\s]+/, (match) => {
     const date = new Date(match);
     if (isFinite(date)) {
-      return date.toString()
+      return date.toString();
     } else {
       return match;
     }
