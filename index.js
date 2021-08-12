eec44965a9d26f13f1e8ea2381723e3da4ed54fc
Jethro Lee <jethro.lee@probit.com>
2021-08-12 16:24:18 +0900

    Implement

--- index.js
+++ index.js
@@ -4,5 +4,13 @@
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
 process.stdin.on('data', (data) => {
+  data = data.replace(/^[^\s]+/, (match) => {
+    const date = new Date(match);
+    if (isFinite(date)) {
+      return date.toString()
+    } else {
+      return match;
+    }
+  });
   process.stdout.write(data);
 });
