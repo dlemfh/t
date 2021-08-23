2c7c70c7803be8c3404d22e1c50e3105522302c9
Jethro Lee <jethro.lee@probit.com>
2021-08-23 18:18:16 +0900

    First field only

--- index.js
+++ index.js
@@ -7,22 +7,28 @@ const RESET = '\x1b[0m';
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
 process.stdin.on('data', (data) => {
-  data = data.replace(/\S+/g, (match) => {
-    const date = new Date(match);
-    if (isFinite(date)) {
-      return `${DIM}[${date.toLocaleString('ko-kr', {
-        timeZone: 'Asia/Seoul',
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-        hour12: false,
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-      })} (KST)]${RESET}`;
-    } else {
-      return match;
-    }
-  });
-  process.stdout.write(data);
+  process.stdout.write(
+    data
+      .split('\n')
+      .map((line) => {
+        return line.replace(/^\S+/, (match) => {
+          const date = new Date(match);
+          if (isFinite(date)) {
+            return `${DIM}[${date.toLocaleString('ko-kr', {
+              timeZone: 'Asia/Seoul',
+              year: 'numeric',
+              month: '2-digit',
+              day: '2-digit',
+              hour12: false,
+              hour: '2-digit',
+              minute: '2-digit',
+              second: '2-digit',
+            })} (KST)]${RESET}`;
+          } else {
+            return match;
+          }
+        });
+      })
+      .join('\n')
+  );
 });
